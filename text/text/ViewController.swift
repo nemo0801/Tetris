b274daf0909e917973f732f8ca441589975445da
xiangyu <444983493@qq.com>
2017-04-20 10:54:15 +0800

    no message

---
+++ text/text/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  text
+//
+//  Created by JUXIN on 2017/4/20.
+//  Copyright © 2017年 xiangyu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
---
+++ text/text/XYLabel.swift
@@ -0,0 +1,13 @@
+//
+//  XYLable.swift
+//  text
+//
+//  Created by JUXIN on 2017/4/20.
+//  Copyright © 2017年 xiangyu. All rights reserved.
+//
+
+import Foundation
+
+class XYLabel: UILabel {
+  
+}
